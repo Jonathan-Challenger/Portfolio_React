01df92b76a6da1460da7a4b4fb21a0fa73590202
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-30 14:46:48 +0000

    project setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      <p>This is my new portfolio</p>
+    </div>
+  );
+};
+
+export default App;
