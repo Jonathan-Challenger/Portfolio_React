f89347a0099716bea973fe3829c433a4fdd56385
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-30 15:18:28 +0000

    start setting up nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className='App'>
+      <Navbar />
       <h1>Hello World</h1>
       <p>This is my new portfolio</p>
     </div>
