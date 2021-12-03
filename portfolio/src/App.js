d7f0f2c1a5d731f662a4a8e6efb1be77442b8ad1
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-03 11:42:25 +0000

    add landing to app

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppNav from "./components/AppNav";
+import Landing from "./components/Landing";
 
 const App = () => {
   return (
     <div className='App'>
       <AppNav />
-      <h1>Hello World</h1>
-      <p>This is my new portfolio</p>
+      <Landing />
     </div>
   );
 };
