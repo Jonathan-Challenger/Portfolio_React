4a5ca2eca16f96b7b5bcb40bfb7ebafc85b4ac6b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-03 14:10:48 +0000

    add about to app

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppNav from "./components/AppNav";
 import Landing from "./components/Landing";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div className='App'>
       <AppNav />
       <Landing />
+      <About />
     </div>
   );
 };
