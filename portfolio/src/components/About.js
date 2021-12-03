abc85a23c4ecb7c70ca96294c179d95dabb360b7
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-03 14:10:35 +0000

    create about skele

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,13 @@
+import { Button, Container } from "reactstrap";
+
+const About = () => {
+  return (
+    <section id='about'>
+      <Container className='text-white' id='about-content'>
+        <h2>About</h2>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
