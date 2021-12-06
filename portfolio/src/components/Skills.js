fe9a060872b3fb547bc67bd8778efb1caaa0f9f7
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-06 13:46:35 +0000

    create skills skeleton

---
+++ portfolio/src/components/Skills.js
@@ -0,0 +1,13 @@
+import { Container } from "reactstrap";
+
+const Skills = () => {
+  return (
+    <section id='skills'>
+      <Container id='skills-content'>
+        <h2 className='section-title my-3'>Skills</h2>
+      </Container>
+    </section>
+  );
+};
+
+export default Skills;
