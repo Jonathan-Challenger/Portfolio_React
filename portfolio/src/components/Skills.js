045c9fadd3f30c7530da9e358f8e1ade1940d968
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-06 14:22:33 +0000

    add card items and some style to skills

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,10 +1,72 @@
-import { Container } from "reactstrap";
+import { Container, Card, CardBody, CardText, CardTitle } from "reactstrap";
 
 const Skills = () => {
   return (
     <section id='skills'>
       <Container id='skills-content'>
-        <h2 className='section-title my-3'>Skills</h2>
+        <h2 className='section-title my-5'>Skills</h2>
+        <div className='skills-info'>
+          <div className='card-item mx-1'>
+            <Card color='danger'>
+              <CardBody>
+                <CardTitle tag='h5'>Card title</CardTitle>
+
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-item mx-1'>
+            <Card color='danger' outline>
+              <CardBody>
+                <CardTitle tag='h5'>Card title</CardTitle>
+
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-item mx-1'>
+            <Card color='danger' outline>
+              <CardBody>
+                <CardTitle tag='h5'>Card title</CardTitle>
+
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-item mx-1'>
+            <Card color='danger' outline>
+              <CardBody>
+                <CardTitle tag='h5'>Card title</CardTitle>
+
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-item mx-1'>
+            <Card color='danger' outline>
+              <CardBody>
+                <CardTitle tag='h5'>Card title</CardTitle>
+
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
       </Container>
     </section>
   );
