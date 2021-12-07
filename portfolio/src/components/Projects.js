0423049c0881d385d0eddbc4a8b61d7c403292aa
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-07 15:40:23 +0000

    create skeleton for projects

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,13 @@
+import { Container } from "reactstrap";
+
+const Projects = () => {
+  return (
+    <section id='projects'>
+      <Container className='text-white' id='project-content'>
+        <h2 className='section-title my-3'>Projects</h2>
+      </Container>
+    </section>
+  );
+};
+
+export default Projects;
