13e453cb8c2efa6cc8a87fa26983c16948543f1c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-08 14:43:32 +0000

    create blackbox detail skeleton

---
+++ portfolio/src/components/detail_pages/BlackboxMovies.js
@@ -0,0 +1,13 @@
+import { Container } from "reactstrap";
+
+const BlackboxMovies = () => {
+  return (
+    <section id='blackboxMovies'>
+      <Container className='text-white' id='blackbox-content'>
+        <h2 className='section-title my-3'>Blackbox Movies</h2>
+      </Container>
+    </section>
+  );
+};
+
+export default BlackboxMovies;
