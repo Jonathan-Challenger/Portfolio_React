d98d95145d8070ef6cd3f18882a301aa6be4ebf2
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-08 14:42:48 +0000

    setup react router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppNav from "./components/AppNav";
-import Landing from "./components/Landing";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import Home from "./components/Home";
+import BlackboxMovies from "./components/detail_pages/BlackboxMovies";
 
 const App = () => {
   return (
     <div className='App'>
-      <AppNav />
-      <Landing />
-      <About />
-      <Skills />
-      <Projects />
+      <Router>
+        <AppNav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/projects/blackbox-movies'
+            element={<BlackboxMovies />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 };
