23e285293b842da533ed6fbc6f04db7dbd96ef43
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-08 12:12:09 +0000

    create projects section

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,6 +5,28 @@ const Projects = () => {
     <section id='projects'>
       <Container className='text-white' id='project-content'>
         <h2 className='section-title my-3'>Projects</h2>
+        <div className='projects-info'>
+          <div id='blackbox-movies'>
+            <div className='overlay'>
+              <p className='large'>View Project</p>
+            </div>
+          </div>
+          <div id='premier-league-stats'>
+            <div className='overlay'>
+              <p className='large'>View Project</p>
+            </div>
+          </div>
+          <div id='weather-app'>
+            <div className='overlay'>
+              <p className='large'>View Project</p>
+            </div>
+          </div>
+          <div id='weblog'>
+            <div className='overlay'>
+              <p className='large'>View Project</p>
+            </div>
+          </div>
+        </div>
       </Container>
     </section>
   );
