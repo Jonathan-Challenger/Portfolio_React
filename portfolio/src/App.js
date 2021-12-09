6cf9d0594ab97eb65b6340cddb0799df77493e83
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-09 14:35:26 +0000

    add detail page routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppNav from "./components/AppNav";
 import Home from "./components/Home";
 import BlackboxMovies from "./components/detail_pages/BlackboxMovies";
+import PLStats from "./components/detail_pages/PLStats";
+import WeatherApp from "./components/detail_pages/WeatherApp";
+import Weblog from "./components/detail_pages/Weblog";
 
 const App = () => {
   return (
@@ -16,6 +19,9 @@ const App = () => {
             path='/projects/blackbox-movies'
             element={<BlackboxMovies />}
           />
+          <Route path='/projects/PL-stats' element={<PLStats />} />
+          <Route path='/projects/weather-app' element={<WeatherApp />} />
+          <Route path='/projects/weblog' element={<Weblog />} />
         </Routes>
       </Router>
     </div>
