9e555083b58013806fb90bdf422256f738ae42f2
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-12-10 11:13:34 +0000

    create contact skeleton

---
+++ portfolio/src/components/Contact.js
@@ -0,0 +1,13 @@
+import { Button, Container } from "reactstrap";
+
+const Contact = () => {
+  return (
+    <section id='contact'>
+      <Container className='text-white' id='contact-content'>
+        <h2 className='section-title my-3'>Contact</h2>
+      </Container>
+    </section>
+  );
+};
+
+export default Contact;
